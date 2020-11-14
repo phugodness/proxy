a05317e4ff3eae1797edcb25b8e89fafaae0c6bb
Truong Van Phu <phugodness@gmail.com>
2020-11-14 16:43:23 +0700

    init

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,16 @@ const express = require('express');
 const axios = require('axios');
 const router = express.Router();
 
-/* GET users li ssting. */
-router.get('/', async function(req, res, next) {
-  const { data } = await axios.get('http://localhost:3000/users');
-  res.json(data);
-  // res.json([]);
-  // return axios
-  //   .get('http://localhost:3000/users')
-  //   .then(response => res.json(response))
+/* GET users lissting. */
+router.get('/', async function (req, res, next) {
+  try {
+    console.log('aaa');
+    const { data } = await axios.get('http://backend:3000/users');
+    return res.json(data);
+  } catch (error) {
+    console.log(error);
+    res.json([{ error: 'not found' }]);
+  }
 });
 
 module.exports = router;
