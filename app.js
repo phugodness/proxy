75c775e38dcbe80dd574e4e97cf258d50716bb03
Truong Van Phu <phugodness@gmail.com>
2020-11-28 17:33:41 +0700

    add redis

--- app.js
+++ app.js
@@ -3,21 +3,30 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const cors = require('cors');
-const { createProxyMiddleware } = require('http-proxy-middleware')
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const redis = require('redis');
 
 const app = express();
-app.use(cors());
+const client = redis.createClient({ host: 'redis' });
+client.set('visits', 0);
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+  client.get('visits', (err, visits) => {
+    console.log({ visits });
+    res.send('Number of visits is: ' + ++visits)
+    client.set('visits', visits)
+  })
+});
 app.use('/', indexRouter);
 
 const onProxyReq = (proxyReq, req, res) => {
